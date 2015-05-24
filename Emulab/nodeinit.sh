5b125ad36c72df6b8d9720f30dcdd3ec30f33163
Shawn <sschenshawn@gmail.com>
2015-05-23 23:07:38 -0400

    added documentation

--- Emulab/nodeinit.sh
+++ Emulab/nodeinit.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Copyright (C) 2014 University of Virginia. All rights reserved.
+#
+# @file      nodeinit.sh
+# @author    Shawn Chen <sc7cq@virginia.edu>
+# @version   1.0
+# @date      May 23, 2015
+#
+# @section   LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or（at your option）
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# @brief    Emulab node initialization
+
+
 sudo yum -y update
 sudo rpm -ivh ~shawn/nuttcp-6.1.2-3.3.x86_64.rpm
 sudo cp ~shawn/devtools-1.1.repo /etc
@@ -10,3 +32,5 @@ git clone https://github.com/Unidata/vcmtp.git
 git clone https://github.com/shawnsschen/CC-NIE-Toolbox.git
 sudo cp ~shawn/CC-NIE-Toolbox/generic/tc/sysctl.conf /etc
 sudo sysctl -p
+#sudo ethtool -K em4 tso off
+#sudo ethtool -K em4 lro off
