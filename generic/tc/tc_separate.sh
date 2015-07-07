18a9d30392132f1449eb84b739802a524fc90c57
Shawn <sschenshawn@gmail.com>
2015-07-07 01:02:57 -0400

    automatic setup tc qdisc and filter

--- generic/tc/tc_separate.sh
+++ generic/tc/tc_separate.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # Hierarchical tc rate limiter. It limits the traffic that matches the tc
 # filter rules using TBF. For the other traffic that does not match, it is
-# served by pfifo_fast without any limit.
+# served by pfifo without any limit.
 # Usage: sudo sh tbfrate.sh add ethN 10mbit 10kb 2ms
 
-export NIC=xxxx
+const="inet addr:"
+bindip=`hostname -I | awk -F ' ' '{print $2}'`
+iface=$(ifconfig | grep -B1 "$const$bindip" | awk '$1!="inet" && $1!="--" {print $1}')
+export NIC=$iface
 tc qdisc add dev $NIC root handle 1: prio
 tc qdisc add dev $NIC parent 1:1 handle 10: tbf rate 50mbit burst 6250 limit 600mb
 tc qdisc add dev $NIC parent 1:2 handle 20: pfifo
