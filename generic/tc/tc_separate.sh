c8f4fe366331ae61ed6a74b0d37504cab6f036fe
Shawn <sschenshawn@gmail.com>
2015-07-07 00:14:03 -0400

    use HZ=1000 for centos and computed a burst

--- generic/tc/tc_separate.sh
+++ generic/tc/tc_separate.sh
@@ -6,7 +6,7 @@
 
 export NIC=xxxx
 tc qdisc add dev $NIC root handle 1: prio
-tc qdisc add dev $NIC parent 1:1 handle 10: tbf rate 50mbit burst 25kb limit 600mb
+tc qdisc add dev $NIC parent 1:1 handle 10: tbf rate 50mbit burst 6250 limit 600mb
 tc qdisc add dev $NIC parent 1:2 handle 20: pfifo_fast
 tc filter add dev $NIC protocol ip parent 1: prio 1 u32 match ip dst 224.0.0.1/32 flowid 1:1
 tc filter add dev $NIC protocol ip parent 1: prio 1 u32 match ip dst 0/0 flowid 1:2
