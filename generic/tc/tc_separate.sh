cd22bebfde494e20ef0bfe928c9f58e176e3745e
Shawn <sschenshawn@gmail.com>
2015-07-07 00:08:58 -0400

    abstract interface name

--- generic/tc/tc_separate.sh
+++ generic/tc/tc_separate.sh
@@ -4,8 +4,9 @@
 # served by pfifo_fast without any limit.
 # Usage: sudo sh tbfrate.sh add ethN 10mbit 10kb 2ms
 
-tc qdisc add dev eth0 root handle 1: prio
-tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate 50mbit burst 25kb limit 600mb
-tc qdisc add dev eth0 parent 1:2 handle 20: pfifo_fast
-tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst 224.0.0.1/32 flowid 1:1
-tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst 0/0 flowid 1:2
+export NIC=xxxx
+tc qdisc add dev $NIC root handle 1: prio
+tc qdisc add dev $NIC parent 1:1 handle 10: tbf rate 50mbit burst 25kb limit 600mb
+tc qdisc add dev $NIC parent 1:2 handle 20: pfifo_fast
+tc filter add dev $NIC protocol ip parent 1: prio 1 u32 match ip dst 224.0.0.1/32 flowid 1:1
+tc filter add dev $NIC protocol ip parent 1: prio 1 u32 match ip dst 0/0 flowid 1:2
