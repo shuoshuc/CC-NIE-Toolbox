d453c59163e64fbe045f76f0280396d324a413fd
Shawn <sschenshawn@gmail.com>
2015-07-20 20:29:38 -0400

    WIP: thru_vs_aggregate.R

---
+++ generic/R/thru_vs_aggregate.R
@@ -0,0 +1,37 @@
+##
+# Copyright (C) 2015 University of Virginia. All rights reserved.
+#
+# @file      thru_vs_aggregate.R
+# @author    Shawn Chen <sc7cq@virginia.edu>
+# @version   1.0
+# @date      July 20, 2015
+#
+# @section   LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or（at your option）
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# @brief     Plot the throughput against aggregate size graph.
+
+
+prefix <- "~/Workspace/VCMTP_LOGS/ExoGENI/july8-2015-UMass-UCD-PSC/PSC/
+           allcsv/csv-1MB/"
+filename <- "Expt1-PSC-run"
+varprefix <- "Expt1_PSC_run"
+thru_1MB <- numeric()
+for(i in 1:10)
+{
+  fullname <- paste(prefix, filename, sep="")
+  varname <- paste(varprefix, i, sep="")
+  oname <- paste(fullname, i, sep="")
+  assign(varname, read.csv(paste(oname, ".csv", sep="")))
+  assign(varname, get(varname)[[5]])
+  thru_1MB <- c(thru_1MB, get(varname))
+}
