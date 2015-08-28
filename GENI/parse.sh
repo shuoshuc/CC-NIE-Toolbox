267ab98dd01a740a8f7dc956fa6724a4c8efc067
Shawn <sschenshawn@gmail.com>
2015-08-28 00:27:55 -0400

    bug fix: missing metadata name reading

--- GENI/parse.sh
+++ GENI/parse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup scl enable python27 'cd ~/vcmtp/VCMTPv3/receiver/logs/ && sh autoproc_pergroup.sh VCMTPv3_RECEIVER_centos_run 20 WAN' &> /dev/null &
+nohup scl enable python27 'cd ~/vcmtp/VCMTPv3/receiver/logs/ && sh autoproc_pergroup.sh day1NGRID.data VCMTPv3_RECEIVER_centos_run 20 WAN' &> /dev/null &
