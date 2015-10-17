2e143fb18df0cd3657f1b63929f6cac0633b13b4
Shawn <sschenshawn@gmail.com>
2015-10-17 19:39:18 -0400

    enhanced to tolerate missing products

--- generic/R/prodsrc-view.R
+++ generic/R/prodsrc-view.R
@@ -1,13 +1,30 @@
-group_size <- 1000
 rates <- numeric()
+total <- 0
+hit <- 0
+agg <- read.csv('~/agg.csv', sep = ',', comment.char = '#')
+start <- agg[[1]]
+end <- agg[[2]]
 data <- read.csv('~/prodsrc-7hr.log', sep = ':', comment.char = '#')
 y <- ifelse(data[[1]] == 'VCMTP', 1, 0)
-groups <- split(y, ceiling(seq_along(y)/group_size))
-for (i in 1:length(groups))
+x <- data[[2]]
+for (i in 1:nrow(agg))
 {
-  rate <- sum(groups[[i]]) / length(groups[[i]]) * 100
-  rates[i] <- rate
-  print(rate)
+  for (j in start[i]:end[i])
+  {
+    row_id <- match(j, x)
+    if (!is.na(row_id))
+    {
+      total <- total + 1
+      if (y[row_id] == 1)
+      {
+        hit = hit + 1
+      }
+    }
+  }
+  rates[i] <- hit / total * 100
+  print(rates[i])
+  total <- 0
+  hit <- 0
 }
 plot(rates, pch=16, type="h", xlab = 'Aggregate group',
      ylab = 'VCMTP-sourced rate (%)')
