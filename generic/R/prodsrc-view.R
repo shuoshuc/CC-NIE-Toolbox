2e7672953ca59e71076060bbd49c7dffdcc805e2
Shawn <sschenshawn@gmail.com>
2015-10-17 20:11:13 -0400

    optimized to speed up

--- generic/R/prodsrc-view.R
+++ generic/R/prodsrc-view.R
@@ -1,6 +1,4 @@
 rates <- numeric()
-total <- 0
-hit <- 0
 agg <- read.csv('~/agg.csv', sep = ',', comment.char = '#')
 start <- agg[[1]]
 end <- agg[[2]]
@@ -9,23 +7,24 @@ y <- ifelse(data[[1]] == 'VCMTP', 1, 0)
 x <- data[[2]]
 for (i in 1:nrow(agg))
 {
-  for (j in start[i]:end[i])
-  {
-    row_id <- match(j, x)
-    if (!is.na(row_id))
-    {
-      total <- total + 1
-      if (y[row_id] == 1)
-      {
-        hit = hit + 1
-      }
-    }
+  start_id <- 0
+  end_id <- 0
+  row_id <- match(start[i], x)
+  if (is.na(row_id)) {
+    start_id <- which.min(abs(x - start[i]))
   }
-  rates[i] <- hit / total * 100
-  print(rates[i])
-  total <- 0
-  hit <- 0
+  else {
+    start_id <- row_id
+  }
+  row_id <- match(end[i], x)
+  if (is.na(row_id)) {
+    end_id <- which.min(abs(x - end[i]))
+  }
+  else {
+    end_id <- row_id
+  }
+  rates[i] <- sum(y[start_id:end_id]) / length(y[start_id:end_id]) * 100
 }
 plot(rates, pch=16, type="h", xlab = 'Aggregate group',
      ylab = 'VCMTP-sourced rate (%)')
-title('Grouped VCMTP-sourced rates')
+title('Grouped VCMTP-sourced rates (7-hr NGRID)')
