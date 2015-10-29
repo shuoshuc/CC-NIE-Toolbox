55cd8783d878b4cc03a3535aba4b2f03dc137f2e
Shawn <sschenshawn@gmail.com>
2015-10-29 13:37:57 -0400

    updated sleep time in insert.sh

--- generic/misc/util/insert.sh
+++ generic/misc/util/insert.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sleep 60
+# recommend sleeping for 30*number of receivers
+# because the initialization is sequential
+sleep 120
 pq_test_insert -f NGRID -v /home/ldm/LDM6-LDM7-comparison/std_data/7hr_NGRID.txt
