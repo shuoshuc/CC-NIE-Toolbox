4de3b7ef13e22fe84bd9f791417f4e94ca13f37b
Shawn <sschenshawn@gmail.com>
2015-11-05 02:14:41 -0500

    added new metric: half-full CBR

--- generic/LogParser/cbr.py
+++ generic/LogParser/cbr.py
@@ -27,7 +27,6 @@ usage     python cbr.py <logfile> <csvfile-to-write>
 
 
 from __future__ import division
-import csv
 import re
 import sys
 import pytz
@@ -36,6 +35,7 @@ from datetime import datetime
 
 # RTT is 55 ms
 rtt = 0.001
+#rtt = 0.055
 
 def parseMLDM(line):
     """Parses the product size and elapsed time received by MLDM.
@@ -135,18 +135,20 @@ def calcCBR(index, complete_dict):
 
     Returns:
         cbr: cumulative bytes ratio
+        half_full_cbr: CBR that reflects the buffer is half full
     """
-    ideal_time = 0
-    true_time  = 0
     # vc_rate is the circuit rate in bps
     vc_rate    = 40000000
-    ideal_time += float(complete_dict[index][0] / vc_rate) + 0.5 * rtt
-    true_time  += complete_dict[index][1]
+    ideal_time = float(complete_dict[index][0] / vc_rate) + 0.5 * rtt
+    half_full_time = float(complete_dict[index][0] / vc_rate) + 0.5 * rtt + 60
+    true_time  = complete_dict[index][1]
     if ideal_time and true_time:
         cbr = float(ideal_time / true_time) * 100
+        half_full_cbr = float(ideal_time / half_full_time) * 100
     else:
         cbr = -1
-    return cbr
+        half_full_cbr = -1
+    return (cbr, half_full_cbr)
 
 
 def main(logfile, csvfile):
@@ -162,11 +164,16 @@ def main(logfile, csvfile):
     """
     (rx_set, rx_dict) = extractLog(logfile)
     w = open(csvfile, 'w+')
-    tmp_str = 'Prodindex, CBR (%)' + '\n'
+    tmp_str = 'Prodindex, CBR (%), half-full-CBR (%), is-half-full' + '\n'
     w.write(tmp_str)
     for i in rx_set:
-        cbr = calcCBR(i, rx_dict)
-        tmp_str = str(i) + ',' + str(cbr) + '\n'
+        (cbr, half_full_cbr) = calcCBR(i, rx_dict)
+        if cbr < half_full_cbr:
+            tmp_str = str(i) + ',' + str(cbr) + ',' + str(half_full_cbr) + \
+            ',' + str(1) + '\n'
+        else:
+            tmp_str = str(i) + ',' + str(cbr) + ',' + str(half_full_cbr) + \
+            ',' + str(0) + '\n'
         w.write(tmp_str)
     w.close()
 
