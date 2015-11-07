33a9db8f8fe1a1bf6b35bb348fa1846c142ccfe8
Shawn <sschenshawn@gmail.com>
2015-11-06 20:48:24 -0500

    uses 1-hr NGRID to speed up the runs

--- generic/misc/util/insert.sh
+++ generic/misc/util/insert.sh
@@ -2,5 +2,5 @@
 
 # recommend sleeping for 30*number of receivers
 # because the initialization is sequential
-sleep 120
-pq_test_insert -f NGRID -v /home/ldm/LDM6-LDM7-comparison/std_data/7hr_NGRID.txt
+sleep 300
+pq_test_insert -f NGRID -v /home/ldm/LDM6-LDM7-comparison/std_data/1hr_NGRID.txt
