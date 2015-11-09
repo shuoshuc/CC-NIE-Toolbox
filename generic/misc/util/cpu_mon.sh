49b9ae8be43d1653057f48236623286299b592fa
Shawn <sschenshawn@gmail.com>
2015-11-09 18:49:21 -0500

    uses uptime to support the low CPU utilization case

--- generic/misc/util/cpu_mon.sh
+++ generic/misc/util/cpu_mon.sh
@@ -4,6 +4,7 @@ LOG=$(printenv LDMHOME)/cpu_measure.log
 while true
 do
     date -u >> $LOG
+    uptime >> $LOG
     ps -eo pcpu,pid,args | grep -E 'ldmd|mldm' >> $LOG
     echo -en "\n\n" >> $LOG
     sleep 60
