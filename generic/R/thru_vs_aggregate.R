e3a683fd33aca11f828d20d128e449c603392cc8
Shawn <sschenshawn@gmail.com>
2015-12-03 01:15:35 -0500

    updated plotting code

--- generic/R/thru_vs_aggregate.R
+++ generic/R/thru_vs_aggregate.R
@@ -40,6 +40,14 @@ for(i in 1:10)
 thru_1MB <- thru_1MB / 1000000
 
 
+thru_1MB <- read.csv('~/recv/csv-1MB/Expt10-UFL-run4.csv')[[5]]/1000000
+thru_10MB <- read.csv('~/recv/csv-10MB/Expt10-UFL-run4.csv')[[5]]/1000000
+thru_20MB <- read.csv('~/recv/csv-20MB/Expt10-UFL-run4.csv')[[5]]/1000000
+thru_50MB <- read.csv('~/recv/csv-50MB/Expt10-UFL-run4.csv')[[5]]/1000000
+thru_100MB <- read.csv('~/recv/csv-100MB/Expt10-UFL-run4.csv')[[5]]/1000000
+thru_200MB <- read.csv('~/recv/csv-200MB/Expt10-UFL-run4.csv')[[5]]/1000000
+thru_500MB <- read.csv('~/recv/csv-500MB/Expt10-UFL-run4.csv')[[5]]/1000000
+thru_1000MB <- read.csv('~/recv/csv-1000MB/Expt10-UFL-run4.csv')[[5]]/1000000
 # For plot
 par(mar=c(5.1,5.1,4.1,2.1))
 boxlist <- list('1'=thru_1MB, '10'=thru_10MB, '20'=thru_20MB, '50'=thru_50MB,
@@ -58,3 +66,13 @@ axis(side=1, at=7, labels='500', cex.axis=1.5)
 axis(side=1, at=8, labels='1000', cex.axis=1.5)
 axis(side=2, at=seq(92,104,2), cex.axis=1.5)
 title(xlab='Aggregate Group Size (MB)', ylab='Throughput per Aggregate (Mbps)', cex.lab=1.5)
+
+boxlist <- list('Group1'=g1, 'Group2'=g2, 'Group3'=g3, 'Group4'=g4, 'Group5'=g5)
+boxplot(boxlist, las=1, outline=FALSE, xaxt='n', yaxt='n')
+axis(side=1, at=1, labels='Group1', cex.axis=1.5)
+axis(side=1, at=2, labels='Group2', cex.axis=1.5)
+axis(side=1, at=3, labels='Group3', cex.axis=1.5)
+axis(side=1, at=4, labels='Group4', cex.axis=1.5)
+axis(side=1, at=5, labels='Group5', cex.axis=1.5)
+axis(side=2, at=seq(95,99,0.2), cex.axis=1.5)
+title(xlab='Aggregate Group Index', ylab='Throughput per 200 MB Aggregate (Mbps)', cex.lab=1.5)
