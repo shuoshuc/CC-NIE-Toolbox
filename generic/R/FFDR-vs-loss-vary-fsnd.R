77a56497d2ba5dc0510b2228d2c8cad3ca7c022b
Shawn <sschenshawn@gmail.com>
2015-12-22 18:48:03 -0500

    updated FFDR graph script

--- generic/R/FFDR-vs-loss-vary-fsnd.R
+++ generic/R/FFDR-vs-loss-vary-fsnd.R
@@ -1,7 +1,5 @@
 par(mar=c(6.1,6.5,4.1,2.1))
 plot(c(0,0.5,1.0,1.5,2.0), m16_5k, type='o', col='blue3', pch=22,
-     xlab='Loss Rate (%)', ylab='FFDR (%)', cex.lab=1.5, cex.axis=1.5, lwd=3)
+     xlab='Packet Loss Rate (%)', ylab='FFDR (%)', cex.lab=1.5, cex.axis=1.5, lwd=3)
 lines(c(0,0.5,1.0,1.5,2.0), m8_5k, type='o', col='brown1', pch=25, lty=2, lwd=3)
-lines(c(0,1.0,2.0), m8_1m, type='o', col='forestgreen', pch=8, lty=5, lwd=3)
-lines(c(0,1.0,2.0), m16_1m, type='o', col='gold1', pch=1, lty=6, lwd=3)
 grid()
