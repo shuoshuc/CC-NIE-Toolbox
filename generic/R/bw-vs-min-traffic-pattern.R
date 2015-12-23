9897366840bd8ad51cfd6dea8ab53c5267e8e3ce
Shawn <sschenshawn@gmail.com>
2015-12-22 19:33:02 -0500

    updated bw-vs-min-traffic graph script

--- generic/R/bw-vs-min-traffic-pattern.R
+++ generic/R/bw-vs-min-traffic-pattern.R
@@ -1,8 +1,8 @@
 # generate plot as 9 : 13.5
 par(mfrow=c(2,1), mar=c(5,5,4,1))
 plot(1:59, bw6, col='red', type='l', lwd=3, lty=5, xlab='Minutes',
-     ylab='B/W (Mbps)', main='Sender Bandwidth Utilization', cex.lab=1.5,
+     ylab='BW (Mbps)', main='Sender NIC Bandwidth (BW) Utilization', cex.lab=1.5,
      cex.axis=1.5, cex.main=1.5)
 lines(1:59, bw7, col='blue', type='l', lwd=3)
 plot(1:60, data, type='h', lwd=3, xlab='Minutes', ylab='Agg. Size (MB)',
-     main='File-stream Traffic Pattern', cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+     main='Filestream Traffic Pattern', cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
