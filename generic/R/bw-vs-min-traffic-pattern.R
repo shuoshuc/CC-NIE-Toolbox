040823b16a5fd759abfe71413bdfd53b1b8d5360
Shawn <sschenshawn@gmail.com>
2015-12-23 16:04:31 -0500

    update bw-vs-min graph script

--- generic/R/bw-vs-min-traffic-pattern.R
+++ generic/R/bw-vs-min-traffic-pattern.R
@@ -1,7 +1,7 @@
 # generate plot as 9 : 13.5
 par(mfrow=c(2,1), mar=c(5,5,4,1))
 plot(1:59, bw6, col='red', type='l', lwd=3, lty=5, xlab='Minutes',
-     ylab='BW (Mbps)', main='Sender NIC Bandwidth (BW) Utilization', cex.lab=1.5,
+     ylab='BW (Mbps)', main='Sender NIC Bandwidth (BW) Usage', cex.lab=1.5,
      cex.axis=1.5, cex.main=1.5)
 lines(1:59, bw7, col='blue', type='l', lwd=3)
 plot(1:60, data, type='h', lwd=3, xlab='Minutes', ylab='Agg. Size (MB)',
