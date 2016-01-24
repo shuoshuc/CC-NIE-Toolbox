3c7dac193ccb23a92facd090b6cf0267c95941d8
Shawn <sschenshawn@gmail.com>
2016-01-23 21:56:38 -0500

    added header for R script

--- generic/R/bw-vs-min-traffic-pattern.R
+++ generic/R/bw-vs-min-traffic-pattern.R
@@ -1,3 +1,26 @@
+##
+# Copyright (C) 2015 University of Virginia. All rights reserved.
+#
+# @file      bw-vs-min-traffic-pattern.R
+# @author    Shawn Chen <sc7cq@virginia.edu>
+# @version   1.0
+# @date      Jan 23, 2016
+#
+# @section   LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or（at your option）
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# @brief     Plot the bandwidth usage against minutes graph.
+
+
 # generate plot as 9 : 13.5
 par(mfrow=c(2,1), mar=c(5,5,4,1))
 plot(1:59, bw6, col='red', type='l', lwd=3, lty=5, xlab='Minutes',
