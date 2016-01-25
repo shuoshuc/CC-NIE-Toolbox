e7a89e4db785fb29851c81fb7036c82decb897da
Shawn <sschenshawn@gmail.com>
2016-01-24 22:27:22 -0500

    added comments for cpu-vs-recv.R

--- generic/R/cpu-vs-recv.R
+++ generic/R/cpu-vs-recv.R
@@ -1,3 +1,26 @@
+##
+# Copyright (C) 2015 University of Virginia. All rights reserved.
+#
+# @file      cpu-vs-recv.R
+# @author    Shawn Chen <sc7cq@virginia.edu>
+# @version   1.0
+# @date      Jan 24, 2016
+#
+# @section   LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or（at your option）
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# @brief     Plot the CPU usage against number of receivers graph.
+
+
 par(mar=c(6.1,6.5,4.1,2.1))
 plot(c(4,8,12,16,20,24), ldm6_1, type='o', col='blue3', pch=22, lwd=3,
      cex.lab=1.5, cex.axis=1.5, xlab='Number of Receivers',
