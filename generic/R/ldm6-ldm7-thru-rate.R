d43d8dcb0229dbdc5a341f47f453e57bdfd5fbe3
Shawn <sschenshawn@gmail.com>
2016-01-28 14:46:53 -0500

    added header

--- generic/R/ldm6-ldm7-thru-rate.R
+++ generic/R/ldm6-ldm7-thru-rate.R
@@ -1,3 +1,26 @@
+##
+# Copyright (C) 2015 University of Virginia. All rights reserved.
+#
+# @file      ldm6-ldm7-thru-rate.R
+# @author    Shawn Chen <sc7cq@virginia.edu>
+# @version   1.0
+# @date      Jan 28, 2016
+#
+# @section   LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or（at your option）
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# @brief     Plot the throughput against rate graph.
+
+
 csvpath <- '~/Workspace/LDM6-LDM7-LOG/WAN-8nodes-LDM6/WAN-8nodes-10Mbps/csv/'
 ldm6 <- matrix(0, nrow = 6)
 ldm7 <- matrix(0, nrow = 7)
