f0a7f3bc80bbb271de145631a1bccc3c26b29e65
Shawn <sschenshawn@gmail.com>
2016-01-30 22:36:45 -0500

    added comments

--- generic/R/read-thru.R
+++ generic/R/read-thru.R
@@ -1,3 +1,26 @@
+##
+# Copyright (C) 2015 University of Virginia. All rights reserved.
+#
+# @file      read-thru.R
+# @author    Shawn Chen <sc7cq@virginia.edu>
+# @version   1.0
+# @date      Jan 30, 2016
+#
+# @section   LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or（at your option）
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# @brief     Reads throughput values from csv files.
+
+
 csvpath <- '~/Workspace/LDM6-LDM7-LOG/thru-vs-rate/Lossy-16nodes/LDM7/16nodes-60Mbps-fsnd1000000/csv/'
 csvfiles <- list.files(path = csvpath, pattern = '.csv')
 thru_arr <- array()
