5e2d8344c052e06715222ebd6e2d7047014f3edd
Shawn <sschenshawn@gmail.com>
2016-02-08 13:29:45 -0500

    added 1M curve back

--- generic/R/FFDR-vs-loss-vary-fsnd.R
+++ generic/R/FFDR-vs-loss-vary-fsnd.R
@@ -25,4 +25,6 @@ par(mar=c(6.1,6.5,4.1,2.1))
 plot(c(0,0.5,1.0,1.5,2.0), m16_5k, type='o', col='blue3', pch=22,
      xlab='Packet Loss Rate (%)', ylab='FFDR (%)', cex.lab=1.5, cex.axis=1.5, lwd=3)
 lines(c(0,0.5,1.0,1.5,2.0), m8_5k, type='o', col='brown1', pch=25, lty=2, lwd=3)
+lines(c(0,1.0,2.0), m8_1m, type='o', col='gold1', pch=21, lty=4, lwd=3)
+lines(c(0,1.0,2.0), m16_1m, type='o', col='purple', pch=23, lty=6, lwd=3)
 grid()
