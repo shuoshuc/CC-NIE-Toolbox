954fadea3497dde9ddf5cfd25be48a720453a829
Shawn <sschenshawn@gmail.com>
2016-04-12 16:00:17 -0400

    enabled grid() in plotting

--- generic/R/RTT-impact-latency-vs-fileindex.R
+++ generic/R/RTT-impact-latency-vs-fileindex.R
@@ -27,3 +27,4 @@ plot(real_8G[1:200], type='l', col='red', xlim=c(0,250), ylim=c(0,6) ,lwd=3,
 lines(real_500M[1:200], type='l', col='blue', ylim=c(0,6), lwd=3)
 lines(ideal_500M[1:200], type='l', col='green', ylim=c(0,6), lwd=3)
 lines(ideal_8G[1:200], type='l', col='purple', ylim=c(0,6), lwd=3)
+grid()
