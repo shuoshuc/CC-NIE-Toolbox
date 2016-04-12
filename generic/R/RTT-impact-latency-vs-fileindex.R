057deaf631b010763583f431c08642decb1347d7
Shawn <sschenshawn@gmail.com>
2016-04-12 13:25:21 -0400

    added header comments

--- generic/R/RTT-impact-latency-vs-fileindex.R
+++ generic/R/RTT-impact-latency-vs-fileindex.R
@@ -1,3 +1,26 @@
+##
+# Copyright (C) 2016 University of Virginia. All rights reserved.
+#
+# @file      RTT-impact-latency-vs-fileindex.R
+# @author    Shawn Chen <sc7cq@virginia.edu>
+# @version   1.0
+# @date      Apr. 12, 2016
+#
+# @section   LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or（at your option）
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# @brief     Plot the 4 ideal/real latency plots.
+
+
 par(mar=c(6.1,6.5,4.1,2.1))
 plot(real_8G[1:200], type='l', col='red', xlim=c(0,250), ylim=c(0,6) ,lwd=3,
      xlab = 'File Index', ylab = 'Latency (ms)', cex.lab=1.5, cex.axis=1.5)
